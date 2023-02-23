5e4db72dd0fc56995bde173ea4837dfc96eb4862
nand-n <nahom.debele@ienetworksolutions.com>
2023-02-23 10:14:52 +0300

    added the navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Dashboard from './Components/Dashboard';
 import Homepage from './Components/Homepage';
+import Navbar from './Components/Navbar';
 
 function App() {
 
   return (
     <div className="App">
+          <Navbar />
+
       <Router>
       <Routes>
+            
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />        
+            <Route path="/dashboard" element={<Dashboard />} />   
+        {/* </Navbar> */}
+            
       </Routes>
     </Router>
     </div>
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Homepage() {
   return (
-      <div className='w-[400px] h-[400px] '>
-          klasdj;fljasdlfjlasdjflkgut
+      <div className='m-[50px] min-w-[400px] border-r  h-[400px] text-yellow bg-green-50 text-3xl font-bold underline'>
+          
+          <div className='text-justify'>
+              Welcome to Exact Betting api Dashboard
+          </div>
+
     </div>
   )
 }
