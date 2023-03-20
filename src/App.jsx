2c89c33b93fbbc7adb18242e1bde38de669f264d
nand-n <nahom.debele@ienetworksolutions.com>
2023-03-20 20:06:44 +0300

    to be deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Dashboard from './Components/Dashboard';
-import Footer from './Components/Footer';
-import Homepage from './Components/Homepage';
-import Navbar from './Components/Navbar';
-import Setting from './Components/Setting';
-import Register from './Components/User/Register';
-import Signin from './Components/User/Signin';
+import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
+import Account from './Components/Account/account'
+import Climate from './Components/Climate/Wind/Wind'
+// import Wind from './Components/Climate/Wind/Wind'
+import Disease from './Components/Disease/disease'
+import Footer from './Components/Footer/footer'
+import Header from './Components/Header/header'
+import Home from './Components/Home/home';
+// import Home from './Components/Home/home'
+import MarketData from './Components/MarketData/marketData'
+// import Services from './Components/Services/services'
+import Soil from './Components/Soil/soil'
+import Navbar from './Componentss/Navbar';
+import Register from './Componentss/User/Register';
+import SignIn from './Componentss/User/Signin';
+
 
 function App() {
 
   return (
-    <div className="bg-[#021C34]  ">
-      <Router>
-          <Navbar />
-
-      <Routes>    
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />   
-          <Route path="/settings" element={<Setting />} />   
-          <Route path="/signin" element={<Signin />} />  
-        <Route path="/register" element={<Register />} />   
-          
-        </Routes>
-      <Footer />
-        
-      </Router>
-    </div>
+    <>
+       <div className='' >
+        <Header />
+        {/* <Navbar /> */}
+      <main>
+            <Routes >
+        <Route path='/' element={<Home />} />
+            <Route path='/account' element={<Account />} />
+            <Route path='/climate' element={<Climate />} />
+        <Route path='/soil' element={<Soil />} />
+        <Route path='/disease' element={<Disease />} />
+              <Route path='/marketData' element={<MarketData />} />
+            <Route path='/signin' element={<SignIn />} />
+              <Route path='/register' element={<Register />} />
+            
+              
+              
+              </Routes>
+        </main>
+      </div>
+      <Footer/>
+    </>
   )
 }
 
